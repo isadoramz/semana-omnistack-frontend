40054b9e06fa0952481580b6873ea11592ccf927
Isadora Zandonotto <isadoraimz@gmail.com>
2020-05-27 10:38:05 -0300

    First frontend version

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import Logon from './pages/Logon';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
+
+export default function() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Logon} />
+                <Route path="/register" component={Register} />
+                <Route path="/profile" component={Profile} />
+                <Route path="/newIncident" component={NewIncident} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
